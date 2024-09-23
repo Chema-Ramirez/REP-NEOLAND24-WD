619e39c5273714ed4b58264caeb364a4c91e66e7
Jose Ramirez <joseramqui93@gmail.com>
2024-09-23 20:19:31 +0200

    solucionado error ejercicio

--- 01-MODULO_WEB_DESIGN/01-EXERCISES/01-JS_BASIC-ALGORITHMS/EXERCISE.js
+++ 01-MODULO_WEB_DESIGN/01-EXERCISES/01-JS_BASIC-ALGORITHMS/EXERCISE.js
@@ -44,11 +44,15 @@ console.log(toy1.price + toy2.price); //Resultado suma de precios = 48
 
 //*1.4
 let globalBasePrice = 10000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 globalBasePrice = 25000;
-console.log(car1.finalPrice + globalBasePrice); //Resultado = 85000
-console.log(car2.finalPrice + globalBasePrice); //Resultado = 10500
+
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+car1.finalPrice = car1.basePrice + globalBasePrice;
+console.log(car1) 
+
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log(car2)
 
 
 
