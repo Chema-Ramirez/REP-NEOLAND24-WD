fcb8dedb21d27d6234d00c13b8719685e9279a7e
Jose Ramirez <joseramqui93@gmail.com>
2024-09-28 11:13:05 +0200

    Bucles y correcciones

--- 01-MODULO_WEB_DESIGN/01-EXERCISES/01-JS_BASIC-ALGORITHMS/EXERCISE.js
+++ 01-MODULO_WEB_DESIGN/01-EXERCISES/01-JS_BASIC-ALGORITHMS/EXERCISE.js
@@ -2,33 +2,33 @@
 
 
 //? ITERACION #1: VARIABLE --->
-//*1.1
+//*EJERCICIO 1.1
 const myFavoriteHero = "Hulk";
 
 
-//*1.2
+//*EJERCICIO 1.2
 const x = 50;
 
 
-//*1.3
+//*EJERCICIO 1.3
 const h = 5;
 const y = 10;
 
 
-//*1.4
+//*EJERCICIO 1.4
 const z = h + y;
 console.log(z); //Resultado: z = 15
 
 
 
 //? ITERACION #2: VARIABLES AVANZADAS --->
-//*1.1
+//*EJERCICIO 1.1
 const character = {name: "Jack Sparrow", age: 10};
 character.age = 25;
 console.log(character); //Resultado: {name: 'Jack Sparrow', age:25}
 
 
-//*1.2
+//*EJERCICIO 1.2
 let firstName = "Jon";
 let lastName= "Snow";
 let age = 24;
@@ -36,13 +36,13 @@ let frase = `Soy ${firstName} ${lastName}, tengo ${24} años y me gustan los lob
 console.log(frase); //Resultado: 'Soy Jon Snow, tengo 24 años y me gustan los lobos'
 
 
-//*1.3
+//*EJERCICIO 1.3
 const toy1 = {name: "Buss MyYear", price: 19};
 const toy2 = {name: "Rallo mcKing", price: 29};
 console.log(toy1.price + toy2.price); //Resultado suma de precios = 48
 
 
-//*1.4
+//*EJERCICIO 1.4
 let globalBasePrice = 10000;
 globalBasePrice = 25000;
 
@@ -57,35 +57,35 @@ console.log(car2) //Resultado: se ha modificado el valor de basePrice en variabl
 
 
 //? ITERACION #3: OPERADORES --->
-//*1.1
+//*EJERCICIO 1.1
 let a = 10;
 let b = 5
 let t = a * b;
 console.log(t); //Resultado = 50
 
 
-//*1.2
+//*EJERCICIO 1.2
 let d = 10;
 let e = 2;
 let f = d / e;
 console.log(f); //Resultado = 5
 
 
-//*1.3
+//*EJERCICIO 1.3
 let g = 15;
 let r = 9 ;
 let k = g % r;
 console.log(k); //Resultado = 6
 
 
-//*1.4
+//*EJERCICIO 1.4
 let p = 10;
 let j = 5;
 let o = p += j;
 console.log(o); //Resultado = 15
 
 
-//*1.5
+//*EJERCICIO 1.5
 let c = 10;
 let m = 5;
 let i = c *= m;
@@ -94,30 +94,30 @@ console.log(i); //Resultado = 50
 
 
 //? ITERACION #4 ARRAYS --->
-//*1.1
+//*EJERCICIO 1.1
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 console.log(avengers[0]) //Resultado: 'HULK'
 
 
-//*1.2
+//*EJERCICIO 1.2
 const avengers1 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 avengers1[0] = "IRONMAN"
 console.log(avengers1); //Resultado: ['IRONMAN', 'SPIDERMAN', 'BLACK PANTHER']
 
 
-//*1.3
+//*EJERCICIO 1.3
 const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"]
 console.log(avengers2.length); //Resultado = 3
 
 
-//*1.4
+//*EJERCICIO 1.4
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters.push("Morty", "Summer"); 
 console.log(rickAndMortyCharacters); //Resultado: ["Rick", "Beth", "Jerry", "Morty", "Summer"]
 console.log(rickAndMortyCharacters[4]); //Resultado: 'Summer'
 
 
-//*1.5
+//*EJERCICIO 1.5
 const rickAndMortyCharacters1 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 const newCharacters1 = rickAndMortyCharacters.pop(); 
 console.log(newCharacters1) //Resultado: 'Summer' (Se ha eliminado el último elemento)
@@ -125,7 +125,7 @@ console.log(rickAndMortyCharacters1[0]) //Resultado primer elemento: 'Rick'
 console.log(rickAndMortyCharacters1[4]) //Resultado ultimo elemento: 'Summer'
 
 
-//*1.6
+//*EJERCICIO 1.6
 const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 const newCharacters2 = rickAndMortyCharacters2.splice(1,1);
 console.log(newCharacters2); //Resultado: ['Beth'] (Devuelve el elemento eliminado)
@@ -134,7 +134,7 @@ console.log(rickAndMortyCharacters2); //Resultado: ["Rick", "Jerry", "Morty", "S
 
 
 //? ITERACION #5 CONDICIONALES --->
-//*1.1
+//*EJERCICIO 1.1
 const number1 = 10;
 const number2 = 20;
 const number3 = 2;
@@ -166,17 +166,81 @@ if((number2/2 == number1)||(number1/5 == number3)){
 
 
 //? ITERACION #6 BUCLES --->
-//*1.1 
+//*EJERCICIO 1.1 
 let bucleFor = 9;
-
-for(contador = 0; contador <= bucleFor; contador++){
-    console.log(contador) //Resultado: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+for(i = 0; i <= bucleFor; i++){
+    console.log(i) //Resultado: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 };
 
 
-//*1.2
+//*EJERCICIO 1.2
 let bucleFor1 = 9;
-
-for(contador1 = 0; contador1 <= bucleFor1; contador1 % 2 == 0){
-    console.log(contador1)
+for(i = 0; i <= bucleFor1; i++){
+i % 2 == 0;
+console.log(i)
 }
+
+
+//*EJERCICIO 1.3
+let contadorOvejas = 10
+for (let i = 0; i <= contadorOvejas; i++) {
+    console.log(i=1, 'Intentando dormir 🐑🙉');
+    console.log(i=2, 'Intentando dormir 🐑🙉');
+    console.log(i=3, 'Intentando dormir 🐑🙉');
+    console.log(i=4, 'Intentando dormir 🐑🙉');
+    console.log(i=5, 'Intentando dormir 🐑🙉');
+    console.log(i=6, 'Intentando dormir 🐑🙉');
+    console.log(i=7, 'Intentando dormir 🐑🙉');
+    console.log(i=8, 'Intentando dormir 🐑🙉');
+    console.log(i=9, 'Intentando dormir 🐑🙉')
+    console.log(i=10, 'Durmiendo 💀💤');}
+/*Resultado:
+1 Intentando dormir 🐑🙉
+2 Intentando dormir 🐑🙉
+3 Intentando dormir 🐑🙉
+4 Intentando dormir 🐑🙉
+5 Intentando dormir 🐑🙉
+6 Intentando dormir 🐑🙉
+7 Intentando dormir 🐑🙉
+8 Intentando dormir 🐑🙉
+9 Intentando dormir 🐑🙉
+10 Durmiendo 💀💤*/
+
+//// OTRA OPCION EJERCICIO 1.3
+let contadorOvejas1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+for (let e = 0; e < contadorOvejas1.length; e++) {
+    if (contadorOvejas1[e] === 10) {
+        console.log(e+1, 'Durmiendo 💀💤');
+    } else {
+        console.log(e+1, 'Intentando dormir 🐑🙉');
+    }}
+/*Resultado:
+1 Intentando dormir 🐑🙉
+2 Intentando dormir 🐑🙉
+3 Intentando dormir 🐑🙉
+4 Intentando dormir 🐑🙉
+5 Intentando dormir 🐑🙉
+6 Intentando dormir 🐑🙉
+7 Intentando dormir 🐑🙉
+8 Intentando dormir 🐑🙉
+9 Intentando dormir 🐑🙉
+10 Durmiendo 💀💤*/
+
+
+
+
+
+
+/*if (contarOvejas == 0) {console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 1){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 2){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 3){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 4){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 5){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 6){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 7){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 8){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 9){console.log("Intentando dormir🐑🛏️")}
+    else if(contarOvejas == 10){console.log("Dormido 💤💀")}
+    else{console.log("Ya hemos dejado de contar")}*/
+    //setInterval()
